52635fe0ef454ecfc89416e20b148d1e03551106
maevahrs <m.hiersoso972@gmail.com>
2022-12-07 13:52:30 +0100

    first cinematic update string.xml

--- androidApp/src/main/java/sae/escapegame/application/android/MainScreen.kt
+++ androidApp/src/main/java/sae/escapegame/application/android/MainScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import kotlinx.coroutines.launch
@@ -108,7 +109,7 @@ private fun body(navController: NavController) {
                 navController.navigate(Screen.DetailScreen.route)
             },
         ) {
-            Text(text = "To DetailScreen")
+            Text(stringResource(sae.escapegame.application.android.R.string.btnJouer))
         }
 
     }

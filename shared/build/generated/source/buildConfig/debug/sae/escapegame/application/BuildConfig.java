acba501cf8a97a4c3c7a066b4aeb5e70fba12183
maevahrs <m.hiersoso972@gmail.com>
2022-12-07 08:19:13 +0100

    fisrtcinemetic

---
+++ shared/build/generated/source/buildConfig/debug/sae/escapegame/application/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package sae.escapegame.application;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "sae.escapegame.application";
+  public static final String BUILD_TYPE = "debug";
+}

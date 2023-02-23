23d8233b1ed82e7bdccd56c2b6ac5b788c9910ed
Léa Doublet <leadbtcode@gmail.com>
2023-02-23 10:59:37 +0100

    ajout avatar loggy pour aide

--- androidApp/src/main/java/sae/escapegame/application/android/MenuPrincipal/MenuPrincipal.kt
+++ androidApp/src/main/java/sae/escapegame/application/android/MenuPrincipal/MenuPrincipal.kt
@@ -5,14 +5,17 @@ import android.annotation.SuppressLint
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
@@ -69,13 +72,29 @@ fun MenuPrincipal(
         },
 
         bottomBar = {
+
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
                     .fillMaxHeight(),
-                horizontalArrangement = Arrangement.Center,
+                horizontalArrangement = Arrangement
+                    .spacedBy(
+                        space= 50.dp,
+                        alignment= Alignment.CenterHorizontally
+                    ),
                 verticalAlignment = Alignment.Bottom
             ) {
+                Image(
+                painter = painterResource(R.drawable.loggyavatar),
+                contentDescription = "avatarAideLoggy",
+                contentScale = ContentScale.Crop,            // crop the image if it's not a square
+                modifier = Modifier.clickable {
+
+                }
+                    .size(64.dp)
+                    .clip(CircleShape)                       // clip to the circle shape
+                    .border(2.dp, Color.Black, CircleShape)
+                )
                 Image(
                     painter = painterResource(R.drawable.symbole_qr_code),
                     contentDescription = "Qrcode",
